5b2f6020cd3bab9b2c3e4cf09640c51ebb0b22c2
sempereluismi <sempereluismiguel@gmail.com>
2022-11-26 13:51:17 +0000

    version final

---
+++ src/components/opinion/Opinion.vue
@@ -0,0 +1,28 @@
+<template>
+  <form action="#">
+
+    <label for="nombre">Nombre</label>
+    <input type="text" id="nombre" name="nombre">
+
+    <label for="email">email</label>
+    <input type="email" id="email" name="email">
+
+    <label for="personaje">Personaje favorito</label>
+    <input type="text" id="personaje" name="personaje">
+
+    <label for="opinion">Opinion</label>
+    <textarea name="opinion" id="opinion" cols="30" rows="10"/>
+
+    <label for="puntuacion">Puntuacion</label>
+    <input type="number" max="10" min="0" name="puntuacion" id="puntuacion">
+
+    <label for="volverVer">Volverias a ver la película</label>
+    <p>si</p>
+    <input type="radio" name="si" value="si" id="volverVer">
+    <p>no</p>
+    <input type="radio" name="si" value="si" id="volverVer">
+
+    <button>Enviar</button>
+</form>
+</template>
+
--- src/views/OpinionView.vue
+++ src/views/OpinionView.vue
@@ -1,3 +1,14 @@
 <template>
-    
+    <h1>Dejanos tu opinión sobre la película</h1>
+
+    <FormOpinion/>
 </template>
+
+<script>
+import { defineAsyncComponent } from '@vue/runtime-core'
+export default {
+    components: {
+        FormOpinion: defineAsyncComponent(() => import("@/components/opinion/Opinion.vue"))
+    }
+}
+</script>
