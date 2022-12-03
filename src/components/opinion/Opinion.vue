dd07dde26e9c07a9c004029db127d44d50b4fde2
sempereluismi <sempereluismiguel@gmail.com>
2022-12-03 19:36:22 +0000

    formulario arreglado

--- src/components/opinion/Opinion.vue
+++ src/components/opinion/Opinion.vue
@@ -1,28 +1,37 @@
 <template>
   <form action="#">
 
-    <label for="nombre">Nombre</label>
-    <input type="text" id="nombre" name="nombre">
+    <div>
+      <label for="nombre">Nombre</label>
+      <input type="text" id="nombre" name="nombre">
+    </div>
 
-    <label for="email">email</label>
-    <input type="email" id="email" name="email">
-
+    <div>
+      <label for="email">email</label>
+      <input type="email" id="email" name="email">
+    </div>
     <label for="personaje">Personaje favorito</label>
     <input type="text" id="personaje" name="personaje">
-
-    <label for="opinion">Opinion</label>
-    <textarea name="opinion" id="opinion" cols="30" rows="10"/>
-
+    <div>
+      <label for="opinion">Opinion</label>
+      <textarea name="opinion" id="opinion" cols="30" rows="10" />
+    </div>
     <label for="puntuacion">Puntuacion</label>
     <input type="number" max="10" min="0" name="puntuacion" id="puntuacion">
+    <div>
 
-    <label for="volverVer">Volverias a ver la película</label>
-    <p>si</p>
-    <input type="radio" name="si" value="si" id="volverVer">
-    <p>no</p>
-    <input type="radio" name="si" value="si" id="volverVer">
+      <label for="volverVer">Volverias a ver la película</label>
+      <div>
+        <p>si</p>
+        <input type="radio" name="si" value="si" id="volverVer">
+      </div>
 
+      <div>
+        <p>no</p>
+        <input type="radio" name="si" value="si" id="volverVer">
+      </div>
+    </div>
     <button>Enviar</button>
-</form>
+  </form>
 </template>
 
