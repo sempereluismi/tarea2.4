71cf28ec81428f4b3e545204e3eeea87ee20b841
luis miguel <sempereluismiguel@gmail.com>
2023-01-31 10:47:25 +0100

    card hecha

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,38 @@
 <template>
-  
+  <div class="container">
+    <a
+      href="https://www.netflix.com/es/title/80093212?source=35"
+      target="_blank"
+    >
+      <div class="logo">
+        <img src="@/assets/netflix.png" alt="logo" />
+        <h2>NETFLIX</h2>
+      </div>
+
+      <div class="body">
+        <p>Precio: Subcripción</p>
+      </div>
+    </a>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+img {
+  @apply w-[70px];
+}
 
+.container {
+  @apply w-[500px] border-2 border-background rounded-xl;
+}
+.logo {
+  @apply flex items-center justify-center gap-6 text-6xl tracking-[1rem] font-bold text-red-700 py-6 ;
+}
+
+.body {
+  @apply text-4xl tracking-widest text-center py-10;
+}
 </style>
--- src/views/StreamingView.vue
+++ src/views/StreamingView.vue
@@ -1,25 +1,19 @@
 <template>
     <h1>Dónde ver</h1>
 
-    <table>
-        <tr>
-            <td><a href="https://www.netflix.com/es/title/80093212?source=35">Netflix</a></td>
-            <td>Subscripción</td>
-        </tr>
-        <tr>
-            <td><a href="https://play.hbomax.com/page/urn:hbo:page:GYXELTwy7GjeNogEAAASe:type:feature?source=googleHBOMAX&action=open">HBO MAX</a></td>
-            <td>Subscripción</td>
-        </tr>
-        <tr>
-            <td><a href="https://www.primevideo.com/dp/amzn1.dv.gti.0eb51679-3e6d-8d93-e698-d9860fe6e122?autoplay=0&ref_=atv_cf_strg_wb">Amazon Prime</a></td>
-            <td>Subscripción</td>
-        </tr>
-        <tr>
-            <td><a href="https://ver.movistarplus.es/ficha?id=1278461&origen=GGL">Movistar Plus+</a></td>
-            <td>3.99€</td>
-        </tr>
-    </table>
+    <div>
+        <card/>
+    </div>
 </template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+    components: {
+        card: defineAsyncComponent( () => import("@/components/Card.vue"))
+    }
+}
+</script>
 
 <style scoped>
 
