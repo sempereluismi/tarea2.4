9b03220c22b7a0dda0650f564f1e26b379094ef9
luis miguel <sempereluismiguel@gmail.com>
2023-02-08 14:21:52 +0100

    v2.2

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,16 +7,39 @@
     </router-link>
 
     <div class="nav-right">
-      <router-link :to="{ name: 'home' }">inicio</router-link>
+      <router-link :to="{ name: 'home' }">Inicio</router-link>
       <router-link :to="{ name: 'director' }">Director</router-link>
       <router-link :to="{ name: 'personaje' }">Personajes</router-link>
       <router-link :to="{ name: 'streaming' }">Dónde ver</router-link>
       <router-link :to="{ name: 'media' }">Media</router-link>
       <router-link :to="{ name: 'opinion' }">Opinión</router-link>
+    </div>
 
+    <div class="icon" @click="showMenu = !showMenu">
+      <i class="fa-solid fa-bars"></i>
     </div>
   </nav>
+  <transition name="bounce">
+    <div class="mobile" v-if="showMenu">
+        <router-link :to="{ name: 'home' }">Inicio</router-link>
+        <router-link :to="{ name: 'director' }">Director</router-link>
+        <router-link :to="{ name: 'personaje' }">Personajes</router-link>
+        <router-link :to="{ name: 'streaming' }">Dónde ver</router-link>
+        <router-link :to="{ name: 'media' }">Media</router-link>
+        <router-link :to="{ name: 'opinion' }">Opinión</router-link>
+      </div>
+  </transition>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false
+    }
+  }
+}
+</script>
 
 
 <style scoped>
@@ -24,30 +47,37 @@ nav {
   @apply flex justify-between fixed top-0 left-0 right-0 bg-white max-h-max z-50;
 }
 
-.nav-right {
-  @apply flex items-center text-lg;
+.icon {
+  @apply flex justify-center items-center w-[100px] text-2xl cursor-pointer xl:hidden;
 }
 
+.nav-right {
+  @apply hidden xl:flex items-center text-lg;
+}
+
+.mobile {
+  @apply z-50 fixed py-4 bg-white w-full flex flex-col justify-center items-center text-lg gap-10;
+}
 
 .nav-left {
   @apply items-stretch py-4 px-4 flex;
 }
-.nav-right a  {
+.nav-right a, .mobile a  {
   @apply relative mx-4;
 }
 
-.nav-right a::before {
+.nav-right a::before, .mobile a::before {
   @apply content-[''] absolute left-0 bottom-0 w-full h-[2px] bg-background rounded-[4px];
   scale: 0 1;
   transform-origin: left;
   transition: scale 0.25s;
 }
 
-.nav-right a:hover::before {
+.nav-right a:hover::before, .mobile a:hover::before{
   scale: 1;
 }
 
-.nav-right a:hover {
+.nav-right a:hover, .mobile a:hover {
   @apply text-background;
 }
 
@@ -63,5 +93,23 @@ nav {
 .nav-left span {
   @apply text-black text-2xl uppercase font-bold ml-10;
 
+}
+
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
