9a2d95e0dbf43d58e4a8dddd008b66a147c5b8de
sempereluismi <semperegomezluismiguel@gmail.com>
2023-03-10 17:24:04 +0100

    rss añadido

--- src/views/Section1.vue
+++ src/views/Section1.vue
@@ -1,13 +1,33 @@
 <template>
-  
+  <div class="container"></div>
+
+  <section>
+    <h1>Sección 1 Director</h1>
+    <a href="src/rss/section1.xml">noticias del director</a>
+  </section>
 </template>
 
 <script>
-export default {
 
+
+export default {
 }
 </script>
 
-<style>
+<style scoped>
+h1 {
+  @apply text-6xl text-center font-title py-4;
+}
+
+section img {
+  @apply w-[800px] rounded;
+}
+.container {
+  @apply w-full h-[90vh] flex -z-50 bg-[url('@/assets/portada.jpg')] bg-fixed bg-cover items-end;
+}
+
+section {
+  @apply -mt-32 z-50 bg-white rounded-t-[4rem];
+}
 
 </style>
--- src/views/Section2.vue
+++ src/views/Section2.vue
@@ -1,13 +1,33 @@
 <template>
+    <div class="container"></div>
   
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+    <section>
+      <h1>Sección 2 Ocho apellidos catalanes</h1>
+      <a href="src/rss/section2.xml">noticias de la pelicula</a>
+    </section>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+    @apply text-6xl text-center font-title py-4;
+  }
+  
+  section img {
+    @apply w-[800px] rounded;
+  }
+  .container {
+    @apply w-full h-[90vh] flex -z-50 bg-[url('@/assets/portada.jpg')] bg-fixed bg-cover items-end;
+  }
+  
+  section {
+    @apply -mt-32 z-50 bg-white rounded-t-[4rem];
+  }
+  
+  </style>
